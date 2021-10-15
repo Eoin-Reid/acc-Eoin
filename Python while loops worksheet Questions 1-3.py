0438b042fa64a754f0679e719f6b4f74ad25672b
Eoin-Reid <92167924+Eoin-Reid@users.noreply.github.com>
2021-10-15 15:01:52 +0100

    Add files via upload

---
+++ Python while loops worksheet Questions 1-3.py
@@ -0,0 +1,26 @@
+#Question. 1
+n = 1
+number = 0
+
+while n <= 10:
+    number = number + int(input('Enter a number: '))
+    n += 1
+average = number / 10
+print('Your average number is,', average)
+
+#Question 2
+n1 = 1
+numbers = 1
+
+while n1 <= 12:
+    print('24 by', numbers, '=', numbers * 24)
+    n1 += 1
+    numbers += 1
+    
+#Question. 3
+n2 = 2
+while n2 == 2:
+    print('Infinite to the screen')
+    print(bool(n2))
+    
+
