ddb7f6207192a3aadbccc34da3b52c3d72f561d6
Eoin-Reid <92167924+Eoin-Reid@users.noreply.github.com>
2021-11-27 10:51:47 +0000

    Add files via upload

--- Python worksheet 9.py
+++ Python worksheet 9.py
@@ -71,6 +71,7 @@ print('Question 3')
  
 while True:
     list3 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    print(list3)
     print('''1. Append an element
     2. Insert an element
     3. Append a list to the given list
@@ -84,31 +85,50 @@ while True:
     
     if option == 1:
         us = input('Enter something you want to append, ')
-        lapp = list3.append(us)
+        lapp = list3.append(us)#Appendes us to end of list
         print(list3)
     
     elif option == 2:
         place = int(input('Enter where you want exchange it:'))
         insrt = int(input('Enter what you want to insert: '))
-        inst = list3.insert(place, insrt)
+        inst = list3.insert(place, insrt)#substitutes a character for another using its position
         print(list3)
     
     elif option == 3:
         a = input('Enter a list to append: ')
-        list3.append(a)
+        list3.append(a)#appends a list at the end
         print(list3)
     
     elif option == 4:
         x = int(input('Enter what you want to modify:'))
         insrt = int(input('Enter what you want to modify it to: '))
-        list3[x] = insrt
+        list3[x] = insrt#substitutes a character for another using its value
         print(list3)
         
     elif option == 5:
         c = int(input('Enter a charcter to delete: '))
-        del list3[c]
+        del list3[c]#Deletes character from position c
         print(list3)
         
+    elif option == 6:
+        d = int(input('Enter an existing element with a given value to delete: '))
+        list3.remove(d)#Delete all characters with value d
+        print(list3)
+    
+    elif option == 7:
+        list3.sort()
+        print(list3)
+    
+    elif option == 8:
+        list3.sort()
+        list3.reverse()
+        print(list3)
+        
+    elif option == 9:
+        list3.copy
+        print(list3)
+        
+
         
         
     
