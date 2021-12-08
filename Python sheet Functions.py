f04341e782439d00eba2f0ab45e4d17dbc17acf4
Eoin-Reid <92167924+Eoin-Reid@users.noreply.github.com>
2021-12-08 15:16:49 +0000

    Add files via upload

---
+++ Python sheet Functions.py
@@ -0,0 +1,30 @@
+print('Question1 pt1')
+def maxNum():
+    num1= int(input('Enter a number:'))
+    num2= int(input('Enter another number:'))
+    if num1 > num2:
+        maxNum = num1
+    else:
+        maxNum = num2
+    print('Highest number =', maxNum)
+maxNum()
+
+print('Question1 pt2')
+a= int(input('Enter a number:'))
+b= int(input('Enter another number:'))
+
+def maxNum2(a,b):
+    if a > b:
+        maxNum = a
+    else:
+        maxNum = b
+    print('Max Numbers = ', maxNum)
+        
+maxNum2(a,b)
+
+print('Question2 pt 1')
+def productNum():
+    c= int(input('Enter a number:'))
+    d= int(input('Enter another number:'))
+    
+        
