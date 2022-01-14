2eb1fae238feba02acb438e75850cad56d055479
Eoin-Reid <92167924+Eoin-Reid@users.noreply.github.com>
2022-01-14 14:53:37 +0000

    Add files via upload

---
+++ BubbleSort2.py
@@ -0,0 +1,31 @@
+file = open('BubbleSort1.py', 'w')#Bubble Sort v3
+# 1. Initialise an unsorted
+L= [5, 7, 3, 6, 2, 4, 1]
+print("INPUT (initial list): ", L)
+exchange = True
+n = len(L)
+i = 0
+
+file.write(str(L))
+
+# 2. Traverse across every element as long as there are exchanges
+while (i< n) and  exchange:
+    print("BEFORE PASS %d: %s " %(i+1, L))
+    exchange = False
+    # assume that there will be no exchanges
+    # 3. Compare all unsorted adjacent elements
+    for j in range(n-i-1):
+        # 4. if the elements are out of order, then swap them
+        print("%s " %L, end="-> ")
+        if L[j] > L[j+1]:
+            L[j], L[j+1] = L[j+1], L[j]
+            # Canonical swap!
+            exchange = True
+            # we've done an exchange!
+        print("%s " %L)
+    print("AFTER PASS %d: %s " %(i+1, L))
+    i= i+1
+file.write('\n' + str(L))
+
+file.close()
+            # increment the loop counterprint("OUTPUT (sorted list): ", L)
