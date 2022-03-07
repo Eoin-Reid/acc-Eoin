ab7668015575d8a9a69118e9f79dadf7efe1a307
Eoin-Reid <92167924+Eoin-Reid@users.noreply.github.com>
2022-03-07 09:41:12 +0000

    Add files via upload

---
+++ Pyplot.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+x = [1,2,3,4,5]
+fx = [3,5,9,8,7]
+
+plt.scatter(x, fx)
+
+
+#add title.etc
+
+plt.title('test')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
